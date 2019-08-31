21b71a9ca7724fa3dddeb86816f8cc2e023313a9
maroeder71 <54691925+maroeder71@users.noreply.github.com>
2019-08-31 16:38:10 -0700

    Add files via upload
    Variables and array code to capture scores, sort and slice into a new top scores array

---
+++ highscores.js
@@ -0,0 +1,19 @@
+// create variable for score of current player
+var currentScore = 0;
+
+// create variable for name of player. Rename in game
+var currentPlayer = _________
+
+// create open array for scores to be added
+var allScores = [];
+
+// add scores to array
+allScores.push(currentScore += "   " += currentPlayer);
+
+// sort scores high to low
+allScores.sort()
+allScores.reverse();
+
+// new array for top scores, push top 10 to new array
+var topScores = [];
+topScores.push(allScores.slice(0, 9));
