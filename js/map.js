f72fe650abd4c1a4063fa39c6ddacc04a0884fb3
mikeromero4 <mikeromero4@yahoo.com>
2019-08-31 18:03:43 -0700

    tilemap

--- js/map.js
+++ js/map.js
@@ -1,27 +1,40 @@
 function renderMap() {
+
     var rowImages = [
         "images/water-block.png",   // Top row is water
-        "images/stone-block.png",   // Row 1 of 3 of stone
-        "images/stone-block.png",   // Row 2 of 3 of stone
         "images/stone-block.png",   // Row 3 of 3 of stone
         "images/grass-block.png",   // Row 1 of 2 of grass
-        "images/grass-block.png" ,
-        "images/grass-block.png" ,
-        "images/grass-block.png" ,
-        "images/grass-block.png" ,
-        "images/grass-block.png" ,
-        "images/grass-block.png" ,
-        "images/grass-block.png" , // Row 2 of 2 of grass
-      ],
+      ]
+      var map = [
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,0,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+         [ 1,2,2,2,2,1,1,1,1,0],
+      ]
+      var tileMap = map.map((e)=>{
+          let row = e.map((e)=>{
+            return rowImages[e]
+          })
+          return row
+         
+      }),
       numRows = 12,
       numCols = 10,
-      row, col;
+       row, col;
     
     // Before drawing, clear existing canvas
     
     for (row = 0; row < numRows; row++) {
       for (col = 0; col < numCols; col++) {
-        ctx.drawImage(Resources.get(rowImages[row]), col * gameSetting.tileW, row * gameSetting.tileH,gameSetting.tileW,gameSetting.tileH*2);
+        ctx.drawImage(Resources.get(tileMap[row][col]), col * gameSetting.tileW, row * gameSetting.tileH,gameSetting.tileW,gameSetting.tileH*2);
       }
     }
 }
